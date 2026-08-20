d537dc45dd2499c44f5468363d74e59207a37f1a
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-20 13:51:26 +0200

    feat: add auth routing

--- backend/v1/app.js
+++ backend/v1/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const passport = require("passport");
+//const cors = require("cors");
+const { authRouter } = require("./routers/authRouter.js");
 
 require("./config/passport.js");
 
@@ -9,6 +11,9 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
+//app.use(cors({origin:}))
+
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, (err) => {
   if (err) {
