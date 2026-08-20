06c435cd3deb3de9d9c69f5565f6e0210debc9b0
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-20 11:45:26 +0200

    feat: set up basic jwt passport

---
+++ backend/config/passport.js
@@ -0,0 +1,25 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const db = require("../lib/queries");
+require("dotenv").config();
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+};
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await db.getUserById(jwt_payload.sub);
+      if (user) {
+        return done(null, true);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  }),
+);
