512d82961bef671288819e96f4f7d696218e2c73
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-20 13:51:11 +0200

    feat: set up basic auth router

--- backend/v1/routers/authRouter.js
+++ backend/v1/routers/authRouter.js
@@ -1,6 +1,17 @@
 const authController = require("../controllers/authController");
 const { Router } = require("express");
+const passport = require("passport");
 
 const authRouter = Router();
 
-authRouter.get("/");
+authRouter.post("/token", authController.loginUser);
+authRouter.delete("/token", authController.logoutUser);
+authRouter.post("/register", authController.createUser);
+authRouter.get(
+  "/whoami",
+  passport.authenticate(["jwt"], { session: false }),
+  authController.whoAmI,
+);
+authRouter.post("/token/refresh", authController.refreshToken);
+
+module.exports = { authRouter };
