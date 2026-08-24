6056a07e35b6ab9160a0323584014700df1feceb
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-24 13:27:47 +0200

    refactor: use isAuth instead of .authenticate

--- backend/v1/routers/authRouter.js
+++ backend/v1/routers/authRouter.js
@@ -1,17 +1,13 @@
 const authController = require("../controllers/authController");
 const { Router } = require("express");
-const passport = require("passport");
+const { isAuth } = require("../middleware/isAuth");
 
 const authRouter = Router();
 
 authRouter.post("/token", authController.loginUser);
 authRouter.delete("/token", authController.logoutUser);
 authRouter.post("/register", authController.createUser);
-authRouter.get(
-  "/whoami",
-  passport.authenticate(["jwt"], { session: false }),
-  authController.whoAmI,
-);
+authRouter.get("/whoami", isAuth, authController.whoAmI);
 authRouter.post("/token/refresh", authController.refreshToken);
 
 module.exports = { authRouter };
