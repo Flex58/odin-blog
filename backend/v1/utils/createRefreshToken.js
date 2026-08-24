b2b3ccdf2f2186210479940a4207e3eb0c7e225f
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-24 11:15:48 +0200

    fix: fix bug that wasn't setting seconds correctly

--- backend/v1/utils/createRefreshToken.js
+++ backend/v1/utils/createRefreshToken.js
@@ -2,7 +2,8 @@ const db = require("../lib/queries");
 
 exports.createRefreshToken = async (userId) => {
   const maxAge = 60 * 60 * 24 * 14;
-  const expiryDate = new Date().setSeconds(maxAge); //14days
+  const expiryDate = new Date(); //14days
+  expiryDate.setSeconds(maxAge);
   const refreshToken = await db.createRefreshToken(userId, expiryDate);
   return { refreshToken: refreshToken.token, maxAge };
 };
