339a32fb5aa3f2f79dfdee559fa10cf4f23b3e33
flex58 <164533598+Flex58@users.noreply.github.com>
2026-08-31 12:33:17 +0200

    feat: add specific cors rules

--- backend/v1/app.js
+++ backend/v1/app.js
@@ -15,7 +15,7 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
